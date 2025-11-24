bf20d96b07c8e2b51921933156b6da9b7d9f2b17
Troidem <onlinexmax@gmail.com>
2025-11-24 17:17:46 +0300

    fix: flags test for 8xy4

--- chip8/cpu.go
+++ chip8/cpu.go
@@ -183,8 +183,9 @@ func (cpu *Cpu) execute_8xyn(op uint16) {
 
 	case 0x4: // ADD Vx, Vy (with carry)
 		sum := uint16(cpu.v[x]) + uint16(cpu.v[y])
-		cpu.v[0xF] = byte(sum >> 8) // carry bit
-		cpu.v[x] = byte(sum)
+		carry := byte(sum >> 8)
+		cpu.v[x] = byte(sum) // store result FIRST
+		cpu.v[0xF] = carry   // store VF LAST
 
 	case 0x5: // SUB Vx, Vy (Vx = Vx - Vy)
 		cpu.v[0xF] = 0
