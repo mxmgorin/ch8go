ca72a442723ecf2653ac8cc725e301531f475760
Troidem <onlinexmax@gmail.com>
2025-12-14 21:36:33 +0300

    fix: html issues

--- web/sw.js
+++ web/sw.js
@@ -1,13 +1,20 @@
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.protocol !== "http:" && url.protocol !== "https:") {
+    return;
+  }
+
   event.respondWith(
-    caches.open("ch8go-runtime").then(cache =>
-      cache.match(event.request).then(resp => {
-        const fetchPromise = fetch(event.request).then(networkResp => {
+    caches.match(event.request).then((resp) => {
+      return (
+        resp ||
+        fetch(event.request).then(async (networkResp) => {
+          const cache = await caches.open("ch8go-runtime");
           cache.put(event.request, networkResp.clone());
           return networkResp;
-        });
-        return resp || fetchPromise;
-      })
-    )
+        })
+      );
+    }),
   );
 });
